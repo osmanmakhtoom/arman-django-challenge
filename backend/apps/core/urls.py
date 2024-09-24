340b42828b2fd4f0e6666825ec8564d81e97dfa7
Osman Makhtoom <osmanmakhtoom@gmail.com>
2024-09-24 18:20:40 +0330

    backend apps folder structure and .coverage config added

---
+++ backend/apps/core/urls.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="OsmanDev API",
+        default_version="v1",
+        description="Arman Volleyball Ticket API documentation",
+    ),
+    public=False,
+    permission_classes=(permissions.IsAdminUser,),
+)
+
+urlpatterns = [
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("apps.accounts.api.urls"), name="accounts"),
+    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/logout/", TokenBlacklistView.as_view(), name="logout"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
