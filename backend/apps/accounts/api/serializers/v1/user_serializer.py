958f22c49e192f6e0598512efcb88df08161421d
Osman Makhtoom <osmanmakhtoom@gmail.com>
2024-09-24 19:22:12 +0330

    Accounts serializers added

---
+++ backend/apps/accounts/api/serializers/v1/user_serializer.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ImproperlyConfigured
+from rest_framework import serializers
+
+from apps.accounts.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+            }
+        }
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        raise ImproperlyConfigured("You cant do this action")
