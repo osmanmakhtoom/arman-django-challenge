edbb3bc34aa739beff5549a83b909a692847ed55
Osman Makhtoom <osmanmakhtoom@gmail.com>
2024-09-27 20:39:48 +0330

    Tests finalized

--- backend/apps/accounts/tests/test_user_register.py
+++ backend/apps/accounts/tests/test_user_register.py
@@ -1,39 +1,36 @@
-from django.urls import include, path, reverse
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 
-class AccountTests(APITestCase, URLPatternsTestCase):
-    urlpatterns = [
-        path('register/', include('apps.accounts.api.urls')),
-    ]
-
+class AccountTests(APITestCase):
     def test_create_account(self):
         """
         Ensure we can create a new account object.
         """
-        url = reverse('register')
-        data = {'email': 'test@test.com', 'password': '<PASSWORD>'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_OK)
-        self.assertEqual(len(response.data), 1)
+        url = reverse("register-list")
+        data = {"email": "test@test.com", "password": "<PASSWORD>"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 6)
 
     def test_create_account_with_invalid_email(self):
         """
         Ensure we can't create a new account object with an invalid email.
         """
-        url = reverse('register')
-        invalid_data = {'email': 'test@test', 'password': '<PASSWORD>'}
-        invalid_response = self.client.post(url, invalid_data, format='json')
-        self.assertEqual(invalid_response.status_code, status.HTTP_400_OK)
+        url = reverse("register-list")
+        invalid_data = {"email": "test@test", "password": "<PASSWORD>"}
+        invalid_response = self.client.post(url, invalid_data, format="json")
+        self.assertEqual(invalid_response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_account_with_duplicate_email(self):
         """
         Ensure we can't create a new account object with a duplicate email.
         """
-        url = reverse('register')
-        first_email = {'email': 'test@test.com', 'password': '<PASSWORD>'}
+        url = reverse("register-list")
+        first_email = {"email": "test@test.com", "password": "<PASSWORD>"}
         duplicate_email = first_email.copy()
-        first_response = self.client.post(url, first_email, format='json')
-        duplicate_response = self.client.post(url, duplicate_email, format='json')
-        self.assertEqual(first_response.status_code, status.HTTP_201_OK)
-        self.assertEqual(duplicate_response.status_code, status.HTTP_400_OK)
+        first_response = self.client.post(url, first_email, format="json")
+        duplicate_response = self.client.post(url, duplicate_email, format="json")
+        self.assertEqual(first_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(duplicate_response.status_code, status.HTTP_400_BAD_REQUEST)
