f00da19564b55b5a54e250f1ce9191d5985e98f9
Osman Makhtoom <osmanmakhtoom@gmail.com>
2024-09-27 20:31:04 +0330

    cache manager and ip_address helper modules added

---
+++ backend/apps/utils/helpers/user_ip.py
@@ -0,0 +1,9 @@
+def ip_address(request) -> str:
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[-1].strip()
+    elif request.META.get("HTTP_X_REAL_IP"):
+        ip = request.META.get("HTTP_X_REAL_IP")
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
